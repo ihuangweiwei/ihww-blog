bac459795aaea7f93e8948fd456852b28cda1d2c
ihuangweiwei <ihuangweiwei@163.com>
2014-12-17 10:54:47 +0800

    Site updated: 2014-12-17 10:54:47

--- js/instagram.js
+++ js/instagram.js
@@ -1,1 +1,146 @@
-var Instagram=function(){var t=[],e=function(t){for(var e in t)for(var i=0,a=t[e].srclist.length;a>i;i++){var n=t[e].bigSrclist[i],o=new Image;o.src=n}},i=function(t){for(var i in t){for(var a="",n=0,o=t[i].srclist.length;o>n;n++)a+='<li>								<div class="img-box">									<a class="img-bg" rel="example_group" href="'+t[i].bigSrclist[n]+'" title="'+t[i].text[n]+'"></a>									<img lazy-src="'+t[i].srclist[n]+'" alt="">								</div>							</li>';$('<section class="archives album"><h1 class="year">'+t[i].year+"<em>"+t[i].month+'月</em></h1>				<ul class="img-box-ul">'+a+"</ul>				</section>").appendTo($(".instagram"))}$(".instagram").lazyload(),r(),setTimeout(function(){e(t)},3e3),$("a[rel=example_group]").fancybox()},a=function(t){if(t.indexOf("outbound-distilleryimage")>=0){var e=t.match(/outbound-distilleryimage([\s\S]*?)\//)[1],i=t.split("/");return"http://distilleryimage"+e+".ak.instagram.com/"+i[i.length-1]}var a="http://photos-g.ak.instagram.com/hphotos-ak-xpf1/",i=t.split("/");return a+i[i.length-1]},n=function(t){for(var e={},n=0,o=t.length;o>n;n++){var r=new Date(1e3*t[n].created_time),s=r.getFullYear(),l=r.getMonth()+1,c=a(t[n].images.low_resolution.url),u=a(t[n].images.standard_resolution.url),g="";t[n].caption&&t[n].caption.text&&(g=t[n].caption.text);var m=s+"-"+l;e[m]?(e[m].srclist.push(c),e[m].bigSrclist.push(u),e[m].text.push(g)):e[m]={year:s,month:l,srclist:[c],bigSrclist:[u],text:[g]}}i(e)},o=function(e){$(".open-ins").html("图片来自instagram，正在加载中…"),$.ajax({url:e,type:"GET",dataType:"jsonp",success:function(e){if(200==e.meta.code){t=t.concat(e.data);var i=e.pagination.next_url;i?o(i):($(".open-ins").html("图片来自instagram，点此访问"),n(t))}else alert("access_token timeout!")}})},r=function(){if($(document).width()<=600)$(".img-box").css({width:"auto",height:"auto"});else{var t=$(".img-box-ul").width(),e=Math.max(.26*t,157);$(".img-box").width(e).height(e)}},s=function(){$(window).resize(function(){r()})};return{init:function(){var t=$(".instagram").attr("data-client-id"),e=$(".instagram").attr("data-user-id");return t?e?(o("https://api.instagram.com/v1/users/"+e+"/media/recent/?client_id="+t+"&count=100"),void s()):(alert("Didn't set your instagram userid.\nPlease see the info on the console of your brower."),void console.log("Please open 'http://jelled.com/instagram/lookup-user-id' to get your userid.")):(alert("Didn't set your instagram client_id.\nPlease see the info on the console of your brower."),void console.log("Please open 'http://instagram.com/developer/clients/manage/' to get your client-id."))}}}();$(function(){Instagram.init()});
+var Instagram = (function(){
+
+	var _collection = [];
+
+	var preLoad = function(data){
+		for(var em in data){
+			for(var i=0,len=data[em].srclist.length;i<len;i++){
+				var src = data[em].bigSrclist[i];
+				var img = new Image();
+				img.src = src;
+			}
+		}
+	}
+
+	var render = function(data){
+		for(var em in data){
+			var liTmpl = "";
+			for(var i=0,len=data[em].srclist.length;i<len;i++){
+				liTmpl += '<li>\
+								<div class="img-box">\
+									<a class="img-bg" rel="example_group" href="'+data[em].bigSrclist[i]+'" title="'+data[em].text[i]+'"></a>\
+									<img lazy-src="'+data[em].srclist[i]+'" alt="">\
+								</div>\
+							</li>';
+			}
+			$('<section class="archives album"><h1 class="year">'+data[em].year+'<em>'+data[em].month+'月</em></h1>\
+				<ul class="img-box-ul">'+liTmpl+'</ul>\
+				</section>').appendTo($(".instagram"));
+		}
+
+		$(".instagram").lazyload();
+		changeSize();
+
+		setTimeout(function(){
+			preLoad(data);
+		},3000);
+		
+		$("a[rel=example_group]").fancybox();
+	}
+
+	var replacer = function(str){
+		if(str.indexOf("outbound-distilleryimage") >= 0 ){
+			var cdnNum = str.match(/outbound-distilleryimage([\s\S]*?)\//)[1];
+			var arr = str.split("/");
+			return "http://distilleryimage"+cdnNum+".ak.instagram.com/"+arr[arr.length-1];
+		}else{
+			var url = "http://photos-g.ak.instagram.com/hphotos-ak-xpf1/";
+			var arr = str.split("/");
+			return url+arr[arr.length-1];
+		}
+		
+	}
+
+	var ctrler = function(data){
+		var imgObj = {};
+		for(var i=0,len=data.length;i<len;i++){
+			var d = new Date(data[i].created_time*1000);
+			var y = d.getFullYear();
+			var m = d.getMonth()+1;
+			var src = replacer(data[i].images.low_resolution.url);
+			var bigSrc = replacer(data[i].images.standard_resolution.url);
+			var text = "";
+			if(data[i].caption && data[i].caption.text){
+				text = data[i].caption.text;
+			}
+			var key = y+"-"+m;
+			if(imgObj[key]){
+				imgObj[key].srclist.push(src);
+				imgObj[key].bigSrclist.push(bigSrc);
+				imgObj[key].text.push(text);
+			}else{
+				imgObj[key] = {
+					year:y,
+					month:m,
+					srclist:[src],
+					bigSrclist:[bigSrc],
+					text:[text]
+				}
+			}
+		}
+		render(imgObj);
+	}
+
+	var getList = function(url){
+		$(".open-ins").html("图片来自instagram，正在加载中…");
+		$.ajax({
+			url: url,
+			type:"GET",
+			dataType:"jsonp",
+			success:function(re){
+				if(re.meta.code == 200){
+					_collection = _collection.concat(re.data);
+					var next = re.pagination.next_url;
+					if(next){
+						getList(next);
+					}else{
+						$(".open-ins").html("图片来自instagram，点此访问");
+						ctrler(_collection);
+					}
+				}else{
+					alert("access_token timeout!");
+				}
+			}
+		});
+	}
+	
+
+	var changeSize = function(){	
+		if($(document).width() <= 600){
+			$(".img-box").css({"width":"auto", "height":"auto"});
+		}else{
+			var width = $(".img-box-ul").width();
+			var size = Math.max(width*0.26, 157);
+			$(".img-box").width(size).height(size);
+		}
+	}
+
+	var bind = function(){
+		$(window).resize(function(){
+			changeSize();
+		});
+	}
+
+	return {
+		init:function(){
+			//getList("https://api.instagram.com/v1/users/438522285/media/recent/?access_token=438522285.2082eef.ead70f432f444a2e8b1b341617637bf6&count=100");
+			var insid = $(".instagram").attr("data-client-id");
+			var userid = $(".instagram").attr("data-user-id");
+			if(!insid){
+				alert("Didn't set your instagram client_id.\nPlease see the info on the console of your brower.");
+				console.log("Please open 'http://instagram.com/developer/clients/manage/' to get your client-id.");
+				return;
+			}
+			if(!userid){
+				alert("Didn't set your instagram userid.\nPlease see the info on the console of your brower.");
+				console.log("Please open 'http://jelled.com/instagram/lookup-user-id' to get your userid.");
+				return;
+			}
+			getList("https://api.instagram.com/v1/users/"+userid+"/media/recent/?client_id="+insid+"&count=100");
+			bind();
+		}
+	}
+})();
+$(function(){
+	Instagram.init();
+})
--- js/main.js
+++ js/main.js
@@ -1,1 +1,119 @@
-var Tips=function(){var n=$(".tips-box"),i=function(){};return i(),{show:function(){n.removeClass("hide")},hide:function(){n.addClass("hide")},init:function(){}}}(),Main=function(){var n=function(){var n=$(".tagcloud a");n.css({"font-size":"12px"});for(var i=0,t=n.length;t>i;i++){var e=parseInt(5*Math.random()+1);n[i].className="",n.eq(i).addClass("color"+e)}},i=function(n){var i=$(".switch-wrap");i.css({transform:"translate(-"+100*n+"%, 0 )"}),$(".icon-wrap").addClass("hide"),$(".icon-wrap").eq(n).removeClass("hide")},t=function(){var t=$("#myonoffswitch"),e=$(".second-part"),a=$(".first-part");t.click(function(){t.hasClass("clicked")?(t.removeClass("clicked"),e.removeClass("turn-left"),a.removeClass("turn-left")):(t.addClass("clicked"),e.addClass("turn-left"),a.addClass("turn-left"),n())});var s=!1,r=!1;$(".icon").bind("mouseenter",function(){s=!0,Tips.show()}).bind("mouseleave",function(){s=!1,setTimeout(function(){r||Tips.hide()},100)}),$(".tips-box").bind("mouseenter",function(){r=!0,Tips.show()}).bind("mouseleave",function(){r=!1,setTimeout(function(){s||Tips.hide()},100)}),$(".tips-inner li").bind("click",function(){var n=$(this).index();i(n),Tips.hide()})},e=function(){var n=$(".isFancy");if(0!=n.length){for(var i=$(".article-inner img"),t=0,e=i.length;e>t;t++){var a=i.eq(t).attr("src"),s=i.eq(t).attr("alt");i.eq(t).replaceWith("<a href='"+a+"' title='"+s+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+a+"' title='"+s+"'></a>")}$(".article-inner .fancy-ctn").fancybox()}};return{init:function(){n(),t(),e(),Tips.init()}}}();$(Main.init());
+var Tips = (function(){
+
+	var $tipBox = $(".tips-box");
+
+	var bind = function(){
+
+	}
+
+	bind();
+	return {
+		show: function(){
+			$tipBox.removeClass("hide");
+		},
+		hide: function(){
+			$tipBox.addClass("hide");
+		},
+		init: function(){
+			
+		}
+	}
+})();
+
+var Main = (function(){
+
+	var resetTags = function(){
+		var tags = $(".tagcloud a");
+		tags.css({"font-size": "12px"});
+		for(var i=0,len=tags.length; i<len; i++){
+			var num = parseInt(Math.random()*5+1);
+			tags[i].className = "";
+			tags.eq(i).addClass("color"+num);
+		}
+	}
+
+	var slide = function(idx){
+		var $wrap = $(".switch-wrap");
+		$wrap.css({
+			"transform": "translate(-"+idx*100+"%, 0 )"
+		});
+		$(".icon-wrap").addClass("hide");
+		$(".icon-wrap").eq(idx).removeClass("hide");
+	}
+
+	var bind = function(){
+		var switchBtn = $("#myonoffswitch");
+		var tagcloud = $(".second-part");
+		var navDiv = $(".first-part");
+		switchBtn.click(function(){
+			if(switchBtn.hasClass("clicked")){
+				switchBtn.removeClass("clicked");
+				tagcloud.removeClass("turn-left");
+				navDiv.removeClass("turn-left");
+			}else{
+				switchBtn.addClass("clicked");
+				tagcloud.addClass("turn-left");
+				navDiv.addClass("turn-left");
+				resetTags();
+			}
+		});
+
+		var timeout;
+		var isEnterBtn = false;
+		var isEnterTips = false;
+
+		$(".icon").bind("mouseenter", function(){
+			isEnterBtn = true;
+			Tips.show();
+		}).bind("mouseleave", function(){
+			isEnterBtn = false;
+			setTimeout(function(){
+				if(!isEnterTips){
+					Tips.hide();
+				}
+			}, 100);
+		});
+
+		$(".tips-box").bind("mouseenter", function(){
+			isEnterTips = true;
+			Tips.show();
+		}).bind("mouseleave", function(){
+			isEnterTips = false;
+			setTimeout(function(){
+				if(!isEnterBtn){
+					Tips.hide();
+				}
+			}, 100);
+		});
+
+		$(".tips-inner li").bind("click", function(){
+			var idx = $(this).index();
+			slide(idx);
+			Tips.hide();
+		});
+	}
+
+	var fancyInit = function(){
+		var isFancy = $(".isFancy");
+		if(isFancy.length != 0){
+			var imgArr = $(".article-inner img");
+			for(var i=0,len=imgArr.length;i<len;i++){
+				var src = imgArr.eq(i).attr("src");
+				var title = imgArr.eq(i).attr("alt");
+				imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
+			}
+			$(".article-inner .fancy-ctn").fancybox();
+		}
+	}
+
+	return {
+		init: function(){
+			resetTags();
+			bind();
+			fancyInit();
+			Tips.init();
+		}
+	}
+})();
+
+$(Main.init());
